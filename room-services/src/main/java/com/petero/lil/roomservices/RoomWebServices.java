4fc4e43e5ab348210fdad1af29c138c6228d2f9a
Peter <ipeterokolocha@gmail.com>
2022-06-15 01:33:33 -0400

    Configured all services without config

---
+++ room-services/src/main/java/com/petero/lil/roomservices/RoomWebServices.java
@@ -0,0 +1,29 @@
+package com.petero.lil.roomservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomWebServices {
+
+	private final RoomRepository repository;
+	
+	@Autowired
+	public RoomWebServices(RoomRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping
+	public Iterable<Room> getAllRooms(){
+		return this.repository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Room getRoom(@PathVariable long id) {
+		return this.repository.findById(id).get();
+	}
+}
